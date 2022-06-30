916725d666747f899c3a3d350282516e8de7f664
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2022-06-30 17:38:19 +0800

    feat:增加点击事件

--- 团队项目/JS/clickEvent.js
+++ 团队项目/JS/clickEvent.js
@@ -8,9 +8,11 @@ function reduceNumber() {
             if (value >= 1) {
                 parentElement.querySelector('.summary').innerHTML = value
                 total(parentElement)
+                summary()
             } else {
                 let delElement = parentElement.closest('.goodsList')
                 parentElement.closest('.goods').removeChild(delElement)
+                updateAll()
             }
         }
     }
@@ -25,12 +27,35 @@ function addNumber() {
             value += 1
             parentElement.querySelector('.summary').innerHTML = value
             total(parentElement)
+            summary()
         }
     }
 }
 
 function total(parentElement) {
-    let price = parseFloat(parentElement.closest('.goodsList').querySelector('.price').innerHTML.split('¥')[1])
+    let price = parseInt(parentElement.closest('.goodsList').querySelector('.price').innerHTML.split('¥')[1])
     let number = parseInt(parentElement.closest('.goodsList').querySelector('.summary').innerHTML)
     parentElement.closest('.goodsList').querySelector('.total').innerHTML = '¥' + (price * number)
+}
+
+function updateAll () {
+    let son = document.querySelectorAll('.goodsList input[type="checkbox"]').length
+    let checkedSon = document.querySelectorAll('.goodsList input[type="checkbox"]:checked').length
+    document.querySelector('.goods .settle .item:first-child .min-item:first-child input[type="checkbox"]')
+        .checked = son!==0&&checkedSon === son;
+}
+
+function summary () {
+    let number = 0;
+    let summaryMoney = 0;
+    let checkedSon = document.querySelectorAll('.goodsList [type="checkbox"]:checked');
+    for (let i = 0; i < checkedSon.length; i++) {
+        let checkedElement = checkedSon[i].closest('.goodsList');
+        number+=parseInt(checkedElement.querySelector('.summary').innerHTML);
+        summaryMoney+=parseFloat(checkedElement.querySelector('.total').innerHTML.split('¥')[1])
+    }
+    document.querySelector('.goods .settle .item:last-child .min-item:first-child span:first-child font')
+        .innerHTML = number
+    document.querySelector('.goods .settle .item:last-child .min-item:first-child span:last-child font')
+        .innerHTML = '¥' + summaryMoney
 }
