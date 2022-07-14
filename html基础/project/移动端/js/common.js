83b3e1ac2f2e0bd2f80e9ff0bb29c11352f7c2ce
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2022-07-14 15:47:44 +0800

    feat:合并网页

--- html基础/project/移动端/js/common.js
+++ html基础/project/移动端/js/common.js
@@ -24,3 +24,28 @@ async function getCinema() {
     return cinema
 }
 
+async function getMoviesType () {
+    let data
+    await $.ajax ({
+        url:`https://www.fastmock.site/mock/bb4157f45a0b5ffdcb3f6d984517a6c0/woniuMovie/getAllTypes`,
+        contentType:'application/json',
+        type:'get',
+        success (res) {
+            data = res
+        }
+    })
+}
+
+async function getOrders () {
+    let data
+    await $.ajax ({
+        url:`https://www.fastmock.site/mock/bb4157f45a0b5ffdcb3f6d984517a6c0/woniuMovie/getOrders
+        `,
+        contentType: 'applcation/json',
+        type: 'get',
+        success (res) {
+            data = res
+        }
+    })
+}
+
